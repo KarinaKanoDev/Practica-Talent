cc9101e76436c2c61b7052eed530a26b6a2c29d1
Mariliz G <marilizgonzalez1@gmail.com>
2021-05-26 14:03:41 -0500

    Corrección de ruteo y agregar párrafo

--- src/Components/Mariliz/Mariliz.jsx
+++ src/Components/Mariliz/Mariliz.jsx
@@ -6,6 +6,7 @@ const Mariliz = () => {
         <Fragment>
             <BotonHomePage />
             <h1> Hola mundo! </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit urna primis, tortor varius a purus rhoncus quam pulvinar integer porta sociis, fringilla quisque semper facilisis convallis accumsan nulla magnis. Fermentum non vivamus convallis fames sem varius sagittis suspendisse fringilla platea, torquent duis iaculis turpis lobortis libero lectus nostra taciti placerat urna, praesent porta condimentum eu id habitant tempor vehicula nullam. Enim eros a cursus viverra laoreet facilisi nibh dictumst, mi conubia curae iaculis fames purus lobortis curabitur, urna interdum cubilia sodales mus pulvinar suspendisse, convallis taciti et quis ultrices hac vitae.</p>
         </Fragment>
      );
 }
