569b077324cd5fc851a431123fb2597bec68976d
Mariliz G <marilizgonzalez1@gmail.com>
2021-05-26 14:30:43 -0500

    Cambios a párrafo y adición de lista

--- src/Components/Mariliz/Mariliz.jsx
+++ src/Components/Mariliz/Mariliz.jsx
@@ -6,7 +6,15 @@ const Mariliz = () => {
         <Fragment>
             <BotonHomePage />
             <h1> Hola mundo! </h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit urna primis, tortor varius a purus rhoncus quam pulvinar integer porta sociis, fringilla quisque semper facilisis convallis accumsan nulla magnis. Fermentum non vivamus convallis fames sem varius sagittis suspendisse fringilla platea, torquent duis iaculis turpis lobortis libero lectus nostra taciti placerat urna, praesent porta condimentum eu id habitant tempor vehicula nullam. Enim eros a cursus viverra laoreet facilisi nibh dictumst, mi conubia curae iaculis fames purus lobortis curabitur, urna interdum cubilia sodales mus pulvinar suspendisse, convallis taciti et quis ultrices hac vitae.</p>
+            <h2> Mi nombre es Mariliz</h2>
+            <p> Y para este proyecto, seré parte de su squad. Por eso me voy a presentar brevemente </p>
+            <div className="favFood">
+                <p> Estas son algunas de las cosas que me gusta comer</p>
+                    <li> waffles </li>
+                    <li> sushi </li>
+                    <li> pizza </li>
+            </div>
+            
         </Fragment>
      );
 }
