1329267b7ef325eb5b176a62c9b9759bc6d15241
KarinaKano13 <75265602+KarinaKano13@users.noreply.github.com>
2021-05-26 20:48:37 -0500

    Update to karina component

--- src/Components/Karina/Karina.jsx
+++ src/Components/Karina/Karina.jsx
@@ -7,6 +7,17 @@ const Karina = () => {
             <BotonHomePage />
             <h2>Hola, soy Karina</h2>
             <p>Este es el segundo pull request de prueba</p>
+            <p>Hola, este es el tercer commit</p>
+
+            <div className='select'>
+                <select>
+                    <option>
+                        
+                    </option>
+                </select>
+
+            </div>
+
         </Fragment>
      );
 }
